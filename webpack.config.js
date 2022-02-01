2e5d45379b5cd95f867af744400692a81d25ec1c
KarenMTan <ktann.309@gmail.com>
2022-02-01 15:03:26 -0800

    Format search results

--- webpack.config.js
+++ webpack.config.js
@@ -15,8 +15,12 @@ module.exports = {
       {
         test: /\.css$/,
         use: ["style-loader", "css-loader"]
+      }, 
+      {
+        test: /\.(jpe?g|png|gif|woff|woff2|eot|ttf|svg)(\?[a-z0-9=.]+)?$/,
+        loader: 'url-loader?limit=100000'
       }
-    ]
+    ], 
   },
   resolve: { extensions: ["*", ".js", ".jsx"] },
   output: {
@@ -31,5 +35,5 @@ module.exports = {
     hotOnly: true,
     historyApiFallback: true
   },
-  plugins: [new webpack.HotModuleReplacementPlugin()]
+  plugins: [new webpack.HotModuleReplacementPlugin()], 
 };
