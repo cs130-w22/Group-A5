74995d8cb5160e3b11f53be680c6d2d617f63b1a
KarenMTan <ktann.309@gmail.com>
2022-02-02 11:44:23 -0800

    Change Album Cards to ListItems

--- src/components/AlbumsList.js
+++ src/components/AlbumsList.js
@@ -1,44 +1,35 @@
 import React from 'react';
-import { Card } from 'react-bootstrap';
+import { Card, ListGroup, Image, Button } from 'react-bootstrap';
 import _ from 'lodash';
+import "./../style/AlbumsList.css";
 import music from '../images/music.jpeg';
+
 const AlbumsList = ({ albums }) => {
+  console.log("albums", albums);
   return (
     <React.Fragment>
       {Object.keys(albums).length > 0 && (
         <div className="albums">
-          {albums.items.map((album, index) => {
-            return (
-              <React.Fragment key={index}>
-                <Card style={{ width: '18rem' }}>
-                  <a
-                    target="_blank"
-                    href={album.external_urls.spotify}
-                    rel="noopener noreferrer"
-                    className="card-image-link"
-                  >
-                     {!_.isEmpty(album.images) ? (
-                      <Card.Img
-                        variant="top"
-                        src={album.images[0].url}
-                        alt=""
-                      />
-                    ) : (
-                      <img src={music} alt="" />
-                    )}
-                  </a>
-                  <Card.Body>
-                    <Card.Title>{album.name}</Card.Title>
-                    <Card.Text>
-                      <small>
-                        {album.artists.map((artist) => artist.name).join(', ')}
-                      </small>
-                    </Card.Text>
-                  </Card.Body>
-                </Card>
-              </React.Fragment>
-            );
-          })}
+          <ListGroup id="list-container" className="container-fluid">
+            {albums.items.map((album, index) => {
+              return (
+                <ListGroup.Item className="container-fluid d-flex flex-row justify-content-between align-items-center">
+                  <div className="d-flex flex-row align-items-center">
+                    {!_.isEmpty(album.images) ? (
+                      <Image src={album.images[2].url}/>
+                    ) : <img src={music} alt="" />}
+                    <div className="info">
+                      <div>{album.name}</div>
+                      <div>{album.artists.map((artist) => artist.name).join(', ')}</div>
+                    </div>
+                  </div>
+                  <Button className="form-button" variant="primary" type="button">
+                      Add
+                    </Button>
+                </ListGroup.Item>
+              )
+            })}
+          </ListGroup>
         </div>
       )}
     </React.Fragment>
